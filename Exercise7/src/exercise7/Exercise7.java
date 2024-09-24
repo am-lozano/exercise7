9324958bcf797a0160bf5fcf41eb71dafb92212f
am.lozano <am.lozano@106-18.info2>
2024-09-24 12:19:23 +0200

    corregido

--- Exercise7/src/exercise7/Exercise7.java
+++ Exercise7/src/exercise7/Exercise7.java
@@ -4,6 +4,12 @@ public class Exercise7 {
 
 	public static void main(String[] args) {
 		
+		System.out.println("     *              *");
+		System.out.println("    *  *            *");
+		System.out.println("   *    *           *");
+		System.out.println("  ********          *");
+		System.out.println(" *        *         *");
+		System.out.println("*          *        * * * * ");
 
 	}
 
